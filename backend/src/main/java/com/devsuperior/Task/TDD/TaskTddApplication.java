c0ded7c9e9b2be86652bf681ebc2561ff4cc4ed9
Janeide Rocha <carpediem99@terra.com.brgit config --global user.email carpediem99@terra.com.br>
2022-01-07 20:56:53 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/Task/TDD/TaskTddApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Task.TDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTddApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/Task/TDD/TaskTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Task.TDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
